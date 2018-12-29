62dc1c3c9c541c862ff85fcee306ff64d5353e4f
umarmansoor1 <umar.mansoor1957@gmai.com>
2018-12-29 11:07:26 -0500

    First Impact

---
+++ src/test/java/com/MavenTestNG/BaseClass.java
@@ -0,0 +1,18 @@
+package com.MavenTestNG;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class BaseClass {
+	
+	WebDriver driver;
+	
+	@Test
+	public void setUp() {
+		System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+	}
+
+}
